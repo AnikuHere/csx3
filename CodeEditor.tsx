b61e75da577351faddf302785e404bcb579810fc
Raven/TX3S <122276552+XFydro@users.noreply.github.com>
2025-07-27 15:08:50 +0530

    fix syntax highlighting
    yeaaa i used chatgpt sorri :c

--- CodeEditor.tsx
+++ CodeEditor.tsx
@@ -42,94 +42,120 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
     updateLineNumbers();
   }, [content, currentTheme, isMobile]);
 
-  const syntaxHighlight = (code: string, lang: string) => {
-    let highlightedCode = code;
+const escapeHTML = (str: string) =>
+  str.replace(/&/g, '&amp;')
+     .replace(/</g, '&lt;')
+     .replace(/>/g, '&gt;');
 
-    if (lang === 'javascript' || lang === 'js') {
-      highlightedCode = code
-        .replace(/\b(const|let|var|function|return|if|else|for|while|class|import|export|from|async|await|try|catch|finally)\b/g, '<span style="color: #c586c0">$1</span>')
-        .replace(/\b(console|document|window|Array|Object|String|Number|Boolean|Date|Math|JSON)\b/g, '<span style="color: #4fc1ff">$1</span>')
-        .replace(/"([^"]*)"/g, '<span style="color: #ce9178">"$1"</span>')
-        .replace(/'([^']*)'/g, '<span style="color: #ce9178">\'$1\'</span>')
-        .replace(/\/\/.*$/gm, '<span style="color: #6a9955">$&</span>')
-        .replace(/\/\*[\s\S]*?\*\//g, '<span style="color: #6a9955">$&</span>');
-    }
-    
-    else if (lang === 'python') {
-      highlightedCode = code
-        .replace(/\b(def|class|if|elif|else|for|while|try|except|finally|with|import|from|as|return|yield|lambda|pass|break|continue|global|nonlocal|assert|del|raise|and|or|not|in|is)\b/g, '<span style="color: #c586c0">$1</span>')
-        .replace(/\b(print|len|range|str|int|float|list|dict|tuple|set|bool|type|isinstance|hasattr|getattr|setattr|open|input|enumerate|zip|map|filter|sorted|reversed|sum|min|max|abs|round)\b/g, '<span style="color: #4fc1ff">$1</span>')
-        .replace(/"([^"]*)"/g, '<span style="color: #ce9178">"$1"</span>')
-        .replace(/'([^']*)'/g, '<span style="color: #ce9178">\'$1\'</span>')
-        .replace(/#.*$/gm, '<span style="color: #6a9955">$&</span>');
-    }
-    
-    else if (lang === 'lua') {
-      highlightedCode = code
-        .replace(/\b(function|end|if|then|else|elseif|while|do|for|repeat|until|break|return|local|and|or|not|nil|true|false)\b/g, '<span style="color: #c586c0">$1</span>')
-        .replace(/\b(print|type|tostring|tonumber|pairs|ipairs|next|table|string|math|io|os)\b/g, '<span style="color: #4fc1ff">$1</span>')
-        .replace(/"([^"]*)"/g, '<span style="color: #ce9178">"$1"</span>')
-        .replace(/'([^']*)'/g, '<span style="color: #ce9178">\'$1\'</span>')
-        .replace(/--.*$/gm, '<span style="color: #6a9955">$&</span>');
-    }
-    
-    else if (lang === 'cpp') {
-      highlightedCode = code
-        .replace(/\b(int|float|double|char|bool|void|string|auto|const|static|extern|inline|virtual|public|private|protected|class|struct|enum|namespace|using|template|typename|if|else|for|while|do|switch|case|default|break|continue|return|try|catch|throw|new|delete|sizeof|typedef|union|goto)\b/g, '<span style="color: #c586c0">$1</span>')
-        .replace(/\b(std|cout|cin|endl|vector|string|map|set|list|queue|stack|pair|make_pair|sort|find|begin|end|size|empty|push_back|pop_back|front|back|insert|erase|clear)\b/g, '<span style="color: #4fc1ff">$1</span>')
-        .replace(/"([^"]*)"/g, '<span style="color: #ce9178">"$1"</span>')
-        .replace(/'([^']*)'/g, '<span style="color: #ce9178">\'$1\'</span>')
-        .replace(/\/\/.*$/gm, '<span style="color: #6a9955">$&</span>')
-        .replace(/\/\*[\s\S]*?\*\//g, '<span style="color: #6a9955">$&</span>');
-    }
-    
-    else if (lang === 'csharp') {
-      highlightedCode = code
-        .replace(/\b(using|namespace|class|struct|interface|enum|public|private|protected|internal|static|readonly|const|virtual|override|abstract|sealed|partial|async|await|var|int|string|bool|double|float|decimal|char|byte|short|long|object|void|if|else|for|foreach|while|do|switch|case|default|break|continue|return|try|catch|finally|throw|new|this|base|null|true|false)\b/g, '<span style="color: #c586c0">$1</span>')
-        .replace(/\b(Console|String|Array|List|Dictionary|HashSet|Queue|Stack|StringBuilder|DateTime|TimeSpan|Math|Convert|Parse|ToString|WriteLine|ReadLine|Length|Count|Add|Remove|Contains|Clear|First|Last|Where|Select|OrderBy|GroupBy)\b/g, '<span style="color: #4fc1ff">$1</span>')
-        .replace(/"([^"]*)"/g, '<span style="color: #ce9178">"$1"</span>')
-        .replace(/'([^']*)'/g, '<span style="color: #ce9178">\'$1\'</span>')
-        .replace(/\/\/.*$/gm, '<span style="color: #6a9955">$&</span>')
-        .replace(/\/\*[\s\S]*?\*\//g, '<span style="color: #6a9955">$&</span>');
-    }
-    
-    else if (lang === 'x3') {
-      highlightedCode = code
-        .replace(/\b(if|else|end|while|def|fncend|call|reg|prt|inp|fetch|exit|cls|goto|wait|inc|dec|add|sub|mul|div|mod|pow|sqrt|fastmath|load|flush|reinit)\b/g, '<span style="color: #c586c0">$1</span>')
-        .replace(/\b(terminal|w_file|r_file|a_file|del_file|create_dir|delete_dir|search_file|sys_info|set_env|str_len|dev\.debug)\b/g, '<span style="color: #4fc1ff">$1</span>')
-        .replace(/\$([a-zA-Z_][a-zA-Z0-9_]*)/g, '<span style="color: #dcdcaa">$&</span>')
-        .replace(/(##[a-zA-Z_][a-zA-Z0-9_]*)/g, '<span style="color: #4ec9b0">$1</span>')
-        .replace(/"([^"]*)"/g, '<span style="color: #ce9178">"$1"</span>')
-        .replace(/'([^']*)'/g, '<span style="color: #ce9178">\'$1\'</span>')
-        .replace(/(\/\/.*$|\\\\.*$)/gm, '<span style="color: #6a9955">$1</span>')
-        .replace(/\b(==|!=|>=|<=|>|<|==ic|\|\+\||startswith|contains|&|\|)\b/g, '<span style="color: #d7ba7d">$1</span>')
-        .replace(/\b(int|float|str|bool)\b/g, '<span style="color: #4ec9b0">$1</span>');
-    }
-    
-    else if (lang === 'visual') {
-      highlightedCode = code
-        .replace(/\b(when|if|then|else|repeat|forever|wait|move|turn|say|play|stop|start|end)\b/g, '<span style="color: #c586c0">$1</span>')
-        .replace(/\b(sprite|sound|backdrop|variable|list|clone|broadcast|receive)\b/g, '<span style="color: #4fc1ff">$1</span>')
-        .replace(/"([^"]*)"/g, '<span style="color: #ce9178">"$1"</span>')
-        .replace(/\/\/.*$/gm, '<span style="color: #6a9955">$&</span>');
-    }
-    
-    else if (lang === 'css') {
-      highlightedCode = code
-        .replace(/([a-zA-Z-]+)(\s*:\s*)/g, '<span style="color: #92c5f8">$1</span>$2')
-        .replace(/(#[a-fA-F0-9]{3,6})/g, '<span style="color: #dcdcaa">$1</span>')
-        .replace(/\{|\}/g, '<span style="color: #c586c0">$&</span>')
-        .replace(/\/\*[\s\S]*?\*\//g, '<span style="color: #6a9955">$&</span>');
-    }
-    
-    else if (lang === 'html') {
-      highlightedCode = code
-        .replace(/(<\/?)([a-zA-Z][a-zA-Z0-9]*)(.*?)(>)/g, '<span style="color: #569cd6">$1</span><span style="color: #92c5f8">$2</span><span style="color: #dcdcaa">$3</span><span style="color: #569cd6">$4</span>')
-        .replace(/(<!--[\s\S]*?-->)/g, '<span style="color: #6a9955">$1</span>');
-    }
-    
-    return highlightedCode;
+const syntaxHighlight = (code: string, lang: string) => {
+  let highlightedCode = escapeHTML(code);
+
+  const stringPattern = {
+    double: /"([^"\\]*(\\.[^"\\]*)*)"/g,
+    single: /'([^'\\]*(\\.[^'\\]*)*)'/g
   };
+
+  const commentPatterns = {
+    js: [/\/\/.*$/gm, /\/\*[\s\S]*?\*\//g],
+    py: [/#.*$/gm],
+    lua: [/--.*$/gm],
+    c: [/\/\/.*$/gm, /\/\*[\s\S]*?\*\//g],
+    x3: [/(\/\/.*$|\\\\.*$)/gm],
+    visual: [/\/\/.*$/gm],
+    css: [/\/\*[\s\S]*?\*\//g],
+    html: [/(<!--[\s\S]*?-->)/g]
+  };
+
+  const applyBase = () => {
+    highlightedCode = highlightedCode
+      .replace(stringPattern.double, '<span style="color: #ce9178">"$1"</span>')
+      .replace(stringPattern.single, "<span style=\"color: #ce9178\">'$1'</span>");
+  };
+
+  const applyComments = (commentRegexes: RegExp[]) => {
+    for (const regex of commentRegexes) {
+      highlightedCode = highlightedCode.replace(regex, '<span style="color: #6a9955">$&</span>');
+    }
+  };
+
+  if (lang === 'javascript' || lang === 'js') {
+    applyComments(commentPatterns.js);
+    applyBase();
+    highlightedCode = highlightedCode
+      .replace(/\b(const|let|var|function|return|if|else|for|while|class|import|export|from|async|await|try|catch|finally)\b/g, '<span style="color: #c586c0">$1</span>')
+      .replace(/\b(console|document|window|Array|Object|String|Number|Boolean|Date|Math|JSON)\b/g, '<span style="color: #4fc1ff">$1</span>');
+  }
+
+  else if (lang === 'python') {
+    applyComments(commentPatterns.py);
+    applyBase();
+    highlightedCode = highlightedCode
+      .replace(/\b(def|class|if|elif|else|for|while|try|except|finally|with|import|from|as|return|yield|lambda|pass|break|continue|global|nonlocal|assert|del|raise|and|or|not|in|is)\b/g, '<span style="color: #c586c0">$1</span>')
+      .replace(/\b(print|len|range|str|int|float|list|dict|tuple|set|bool|type|isinstance|hasattr|getattr|setattr|open|input|enumerate|zip|map|filter|sorted|reversed|sum|min|max|abs|round)\b/g, '<span style="color: #4fc1ff">$1</span>');
+  }
+
+  else if (lang === 'lua') {
+    applyComments(commentPatterns.lua);
+    applyBase();
+    highlightedCode = highlightedCode
+      .replace(/\b(function|end|if|then|else|elseif|while|do|for|repeat|until|break|return|local|and|or|not|nil|true|false)\b/g, '<span style="color: #c586c0">$1</span>')
+      .replace(/\b(print|type|tostring|tonumber|pairs|ipairs|next|table|string|math|io|os)\b/g, '<span style="color: #4fc1ff">$1</span>');
+  }
+
+  else if (lang === 'cpp' || lang === 'c') {
+    applyComments(commentPatterns.c);
+    applyBase();
+    highlightedCode = highlightedCode
+      .replace(/\b(int|float|double|char|bool|void|string|auto|const|static|extern|inline|virtual|public|private|protected|class|struct|enum|namespace|using|template|typename|if|else|for|while|do|switch|case|default|break|continue|return|try|catch|throw|new|delete|sizeof|typedef|union|goto)\b/g, '<span style="color: #c586c0">$1</span>')
+      .replace(/\b(std|cout|cin|endl|vector|string|map|set|list|queue|stack|pair|make_pair|sort|find|begin|end|size|empty|push_back|pop_back|front|back|insert|erase|clear)\b/g, '<span style="color: #4fc1ff">$1</span>');
+  }
+
+  else if (lang === 'csharp') {
+    applyComments(commentPatterns.c);
+    applyBase();
+    highlightedCode = highlightedCode
+      .replace(/\b(using|namespace|class|struct|interface|enum|public|private|protected|internal|static|readonly|const|virtual|override|abstract|sealed|partial|async|await|var|int|string|bool|double|float|decimal|char|byte|short|long|object|void|if|else|for|foreach|while|do|switch|case|default|break|continue|return|try|catch|finally|throw|new|this|base|null|true|false)\b/g, '<span style="color: #c586c0">$1</span>')
+      .replace(/\b(Console|String|Array|List|Dictionary|HashSet|Queue|Stack|StringBuilder|DateTime|TimeSpan|Math|Convert|Parse|ToString|WriteLine|ReadLine|Length|Count|Add|Remove|Contains|Clear|First|Last|Where|Select|OrderBy|GroupBy)\b/g, '<span style="color: #4fc1ff">$1</span>');
+  }
+
+  else if (lang === 'x3') {
+    applyComments(commentPatterns.x3);
+    applyBase();
+    highlightedCode = highlightedCode
+      .replace(/(##[a-zA-Z_][a-zA-Z0-9_]*)/g, '<span style="color: #4ec9b0">$1</span>')
+      .replace(/\$[a-zA-Z_][a-zA-Z0-9_]*/g, '<span style="color: #dcdcaa">$&</span>')
+      .replace(/\b(int|float|str|bool)\b/g, '<span style="color: #4ec9b0">$1</span>')
+      .replace(/\b(==|!=|>=|<=|>|<|==ic|\|\+\||startswith|contains|&|\|)\b/g, '<span style="color: #d7ba7d">$1</span>')
+      .replace(/\b(terminal|w_file|r_file|a_file|del_file|create_dir|delete_dir|search_file|sys_info|set_env|str_len|dev\.debug)\b/g, '<span style="color: #4fc1ff">$1</span>')
+      .replace(/\b(if|else|end|while|def|fncend|call|reg|prt|inp|fetch|exit|cls|goto|wait|inc|dec|add|sub|mul|div|mod|pow|sqrt|fastmath|load|flush|reinit)\b/g, '<span style="color: #c586c0">$1</span>');
+  }
+
+  else if (lang === 'visual') {
+    applyComments(commentPatterns.visual);
+    applyBase();
+    highlightedCode = highlightedCode
+      .replace(/\b(when|if|then|else|repeat|forever|wait|move|turn|say|play|stop|start|end)\b/g, '<span style="color: #c586c0">$1</span>')
+      .replace(/\b(sprite|sound|backdrop|variable|list|clone|broadcast|receive)\b/g, '<span style="color: #4fc1ff">$1</span>');
+  }
+
+  else if (lang === 'css') {
+    applyComments(commentPatterns.css);
+    applyBase();
+    highlightedCode = highlightedCode
+      .replace(/([a-zA-Z-]+)(\s*:\s*)/g, '<span style="color: #92c5f8">$1</span>$2')
+      .replace(/(#[a-fA-F0-9]{3,6})/g, '<span style="color: #dcdcaa">$1</span>')
+      .replace(/\{|\}/g, '<span style="color: #c586c0">$&</span>');
+  }
+
+  else if (lang === 'html') {
+    applyComments(commentPatterns.html);
+    highlightedCode = highlightedCode
+      .replace(/(<\/?)([a-zA-Z][a-zA-Z0-9]*)(.*?)(>)/g, '<span style="color: #569cd6">$1</span><span style="color: #92c5f8">$2</span><span style="color: #dcdcaa">$3</span><span style="color: #569cd6">$4</span>');
+  }
+
+  return highlightedCode;
+};
+
 
   const editorStyle = {
     backgroundColor: currentTheme.colors.editor.background,
